439eb99b745eb8136d62f32009c7471a57a27dba
Fuvu Chirwa <fuvu.chirwa@gmail.com>
2013-09-11 15:06:47 +0200

    fixing observatios crashing because of voided concepts

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -41,8 +41,8 @@ class Observation < ActiveRecord::Base
   
   def concept_name=(concept_name)
     self.concept_id = ConceptName.find_by_name(concept_name).concept_id
-    rescue
-      raise "\"#{concept_name}\" does not exist in the concept_name table"
+  rescue
+    raise "\"#{concept_name}\" does not exist in the concept_name table"
   end
 
   def value_coded_or_text=(value_coded_or_text)
@@ -116,15 +116,16 @@ class Observation < ActiveRecord::Base
     return coded_name unless coded_name.blank?
     answer = Concept.find_by_concept_id(self.value_coded).shortname rescue nil
 	
-	if answer.nil?
-		answer = Concept.find_by_concept_id(self.value_coded).fullname rescue nil
-	end
+    if answer.nil?
+      answer = Concept.find_by_concept_id(self.value_coded).fullname rescue nil
+    end
 
-	if answer.nil?
-		answer = Concept.find_with_voided(self.value_coded).fullname + ' - retired'
-	end
+		if answer.nil?
+      answer = Concept.find_with_voided(self.value_coded).fullname rescue ""
+      answer = answer + ' - retired'
+    end
 	
-	return answer
+    return answer
   end
 
   def self.new_accession_number
